89647b1d5075d253fe0e643a82711faf82640a16
Abdulsattar4512 <abdulsattar.shaik@cuw.edu>
2025-02-23 16:06:32 -0600

    Create taskform.js

---
+++ taskform.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const TaskForm = ({ onAddTask }) => {
+  const [taskName, setTaskName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskName) {
+      onAddTask(taskName);
+      setTaskName('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+
+export default TaskForm;
